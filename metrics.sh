63a4177ad8615848aec683f020627937351d9f93
Jorge Frota <jorgepabloafrota@gmail.com>
2024-07-09 23:41:38 -0400

    fix: new metrics script

--- metrics.sh
+++ metrics.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-OUTPUT_FILE="metrics.txt"
+output_file="system_usage.txt"
 
-# Cabeçalho do arquivo de saída
-echo "timestamp,cpu_usage,memory_usage" > $OUTPUT_FILE
-
-# Função para obter métricas
-get_metrics() {
-  timestamp=$(($(date +%s%N)/1000000))
-  cpu_idle=$(vmstat 1 2 | tail -1 | awk '{print $15}')
-  cpu_usage=$(echo "100 - $cpu_idle" | bc)
-  memory_usage=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
-  echo "$timestamp,$cpu_usage,$memory_usage" >> $OUTPUT_FILE
-}
-
-# Loop para coletar métricas a cada 10 segundos
 while true; do
-  get_metrics
-  sleep 5
+    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    cpu_usage=$(mpstat | awk '$12 ~ /[0-9.]+/ { print 100 - $12 }')
+    ram_usage=$(free | awk '/Mem:/ {printf("%.2f%%\n", $3/$2 * 100.0)}')
+    
+    echo "$timestamp, ${cpu_usage}%, $ram_usage" >> "$output_file"
+    sleep 3
 done
-
