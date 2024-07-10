da0c8c877fb2b5ee9ae405defe731f9008d6b622
Jorge Frota <jorgepabloafrota@gmail.com>
2024-07-09 23:48:41 -0400

    test: update load-test

--- metrics.sh
+++ metrics.sh
@@ -2,11 +2,13 @@
 
 output_file="system_usage.txt"
 
+> "$output_file"
+
 while true; do
     timestamp=$(date +"%Y-%m-%d %H:%M:%S")
     cpu_usage=$(mpstat | awk '$12 ~ /[0-9.]+/ { print 100 - $12 }')
     ram_usage=$(free | awk '/Mem:/ {printf("%.2f%%\n", $3/$2 * 100.0)}')
     
-    echo "$timestamp, ${cpu_usage}%, $ram_usage" >> "$output_file"
+    echo "$timestamp, ${cpu_usage}%, $ram_usage" > "$output_file"
     sleep 3
 done
